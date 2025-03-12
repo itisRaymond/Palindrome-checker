0c33cd992e29b65b81992c9a3245dc4725c70194
RayNotFound <159305483+itisRaymond@users.noreply.github.com>
2025-03-12 22:56:00 +0100

    Update palindrome_checker.py
    Added the following features
    	1.	Added support for capitalized letters.
    	2.	Added an option to quit the program.
    	3.	Improved and clarified user messages.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,12 +1,17 @@
+print("Welcome to the GREAT Palindrome checker, not sure if your word is a palindrome??? Come check it here!!! \n")
 
 def palindrome_checker(word):
+    word = word.lower()
     reverse_word = word[::-1]
     if word == reverse_word:
-         print("this is a palindrome")
-    else: print("this is not a palindrome")
+         print("this is a palindrome \n")
+    else: print("this is not a palindrome \n")
     
-for i in range(10):
-    word = input("enter a word ")
+print("*devnote: write 'quit' to quit the program \n")
+while True:
+    word = input("Please enter in a word to check ")
+    if word == "quit":
+        print("the program has stopped \n")
+        break
     palindrome_checker(word)
-
-
+    
